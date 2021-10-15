20650ede156c254cd5ba96cf35ec88ee35281140
JesusAbreuGonzalez <77336425+JesusAbreuGonzalez@users.noreply.github.com>
2021-10-15 18:34:09 -0400

    Creando contexto

---
+++ RegistroPedidosConDetalle/DAL/Contexto.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RegistroPedidosConDetalle.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RegistroPedidosConDetalle.DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Suplidores> Suplidores { get; set; }
+        public DbSet<Productos> Productos { get; set; }
+        public DbSet<Ordenes> Ordenes { get; set; }
+        public DbSet<OrdenesDetalle> OrdenesDetalle { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source = Data/GestionPedidos.db");
+        }
+    }
+}
