a26e0272475bab79186c04b2922f9f86ff94ade0
JesusAbreuGonzalez <77336425+JesusAbreuGonzalez@users.noreply.github.com>
2021-10-15 18:33:59 -0400

    Creando entidades

---
+++ RegistroPedidosConDetalle/Models/Ordenes.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RegistroPedidosConDetalle.Models
+{
+    public class Ordenes
+    {
+        [Key]
+        public int OrdenId { get; set; }
+        public DateTime Fecha { get; set; }
+        public float Monto { get; set; }
+
+        public virtual ICollection<OrdenesDetalle> OrdenesDetalle { get; set; }
+
+        public Ordenes()
+        {
+            OrdenId = 0;
+            Fecha = DateTime.Now;
+            Monto = 0;
+            OrdenesDetalle = new HashSet<OrdenesDetalle>();
+        }
+    }
+}
---
+++ RegistroPedidosConDetalle/Models/OrdenesDetalle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RegistroPedidosConDetalle.Models
+{
+    public class OrdenesDetalle
+    {
+        [Key]
+        public int DetalleId { get; set; }
+        public int OrdenId { get; set; }
+        public int ProductoId { get; set; }
+        public float Cantidad { get; set; }
+        public float Costo { get; set; }
+
+        public OrdenesDetalle()
+        {
+            DetalleId = 0;
+            OrdenId = 0;
+            ProductoId = 0;
+            Cantidad = 0;
+            Costo = 0;
+        }
+    }
+}
